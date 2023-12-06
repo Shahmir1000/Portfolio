1303ecadc4ccb1f8819ec5e46aa87275c04c9444
Muhammad Shahmir Masood <shahmir1000@hotmail.com>
2023-12-06 08:35:45 -0500

    added ProjectCards

---
+++ Project1/ClientApp/src/components/Projects/ProjectCard/ProjectCard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./ProjectCard.css"; // Assuming you have a CSS file for styles
+//import ShahmirImage from "../../Assets/ShahmirWFCU.jfif";
+import PropTypes from "prop-types";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  ListGroup,
+  CardLink,
+  ListGroupItem,
+} from "reactstrap";
+
+/*
+          "id": 1,
+          "companyId": "1",
+          "name": "Admin Tracker",
+          "componentName": "AdminTracker",
+          "description": "Redesigning the corporate website for a modern look and feel.",
+          "status": "Deployed",
+          "startDate": "2023-01-15",
+          "endDate": "2023-04-30",
+          "skillsId": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          "YoutubeLink": "",
+          "GithubLink": "",
+          "ProjectLink": ""
+
+* */
+function ProjectCard({ name, description, skills }) {
+  return (
+    <Card
+      style={{
+        width: "18rem",
+      }}
+    >
+      <img alt="Card" src="https://picsum.photos/300/200" />
+      <CardBody>
+        <CardTitle tag="h5">Card Title</CardTitle>
+        <CardText>This is some text within a card body.</CardText>
+      </CardBody>
+      <ListGroup flush>
+        <ListGroupItem>An item</ListGroupItem>
+        <ListGroupItem>A second item</ListGroupItem>
+        <ListGroupItem>And a third item</ListGroupItem>
+      </ListGroup>
+      <CardBody>
+        <CardLink href="#">Card Link</CardLink>
+        <CardLink href="#">Another Card Link</CardLink>
+      </CardBody>
+    </Card>
+  );
+}
+ProjectCard.prototype = {
+  name: PropTypes.string,
+  descriptions: PropTypes.string,
+  skills: PropTypes.array,
+};
+
+ProjectCard.defaultProps = {
+  name: "No Name",
+  descriptions: "No Description",
+  skills: [],
+};
+
+export default ProjectCard;
--- Project1/ClientApp/src/components/Projects/Projects.js
+++ Project1/ClientApp/src/components/Projects/Projects.js
@@ -1,14 +1,15 @@
-// LandingPage.js
-import React from 'react';
-import './Projects.css'; // Assuming you have a CSS file for styles
-import ShahmirImage from '../../Assets/ShahmirWFCU.jfif';
-import { Container, Col, Row } from 'reactstrap';
-
+// Projects.js
+import React from "react";
+import "./Projects.css"; // Assuming you have a CSS file for styles
+//import ShahmirImage from "../../Assets/ShahmirWFCU.jfif";
+import { Container, Col, Row } from "reactstrap";
+import ProjectCard from "./ProjectCard/ProjectCard";
 function Projects() {
   return (
-    <div>
-        <h1>Projects</h1>
-    </div>
+    <Container>
+      <h1>Projects</h1>
+      <ProjectCard></ProjectCard>
+    </Container>
   );
 }
 
