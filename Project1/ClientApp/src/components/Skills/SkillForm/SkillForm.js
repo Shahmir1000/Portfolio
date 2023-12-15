2a90b06dcceb46af78918cbb9f783152ac6bacfb
Muhammad Shahmir Masood <shahmir1000@hotmail.com>
2023-12-15 01:50:48 -0500

    Displayed skills and added edit functionality

--- Project1/ClientApp/src/components/Skills/SkillForm/SkillForm.js
+++ Project1/ClientApp/src/components/Skills/SkillForm/SkillForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import {
   Form,
   Label,
@@ -6,16 +7,47 @@ import {
   Input,
   Button,
   FormFeedback,
+  Card,
+  CardHeader,
+  ListGroup,
+  ListGroupItem,
+  Row,
+  Col,
+  Container,
+  CardBody,
 } from "reactstrap";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { DevTool } from "@hookform/devtools";
 
-function SkillForm({ id }) {
+function SkillForm({ id, categories, proficiencies, skill }) {
+  const [isEdit, setIsEdit] = useState(
+    skill === undefined || skill === null || id === 0
+  );
+
+  const handleIsEdit = (e) => {
+    setIsEdit(!isEdit);
+  };
+
+  let defaultValues =
+    skill === undefined || skill === null
+      ? { name: "", category: "", proficiency: "" }
+      : {
+          name: skill.name,
+          category: categories?.find((x) => x.id === skill.categoryId)?.name,
+          proficiency: proficiencies?.find((x) => x.id === skill.proficiencyId)
+            ?.name,
+        };
+  console.log(defaultValues);
   const schema = yup.object().shape({
     name: yup.string().trim().nullable().required("Name is required"),
     category: yup.string().trim().nullable().required("Category is required"),
+    proficiency: yup
+      .string()
+      .trim()
+      .nullable()
+      .required("Proficiency is required"),
   });
   const {
     register,
@@ -24,54 +56,114 @@ function SkillForm({ id }) {
     formState: { errors },
     control,
   } = useForm({
+    defaultValues,
     resolver: yupResolver(schema),
   });
 
-  const onSubmit = (data) => console.log(data);
-  console.log(watch("name"));
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   const { ref: nameRef, ...registerName } = register("name");
   const { ref: categoryRef, ...registerCategory } = register("category");
+  const { ref: proficiencyRef, ...registerProficiency } =
+    register("proficiency");
 
   return (
-    <div>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <FormGroup>
-          <Label for={`name${id}`}>Name</Label>
-          <Input
-            id={`name${id}`}
-            name="name"
-            placeholder="Skill Name"
-            type="text"
-            {...registerName}
-            innerRef={nameRef}
-            invalid={!!errors.name}
-          />
-          {errors?.name && (
-            <FormFeedback> {errors?.name?.message}</FormFeedback>
-          )}
-        </FormGroup>
-        <FormGroup>
-          <Label for={`category${id}`}>Category</Label>
-          <Input
-            id={`category${id}`}
-            name="category"
-            placeholder="Category"
-            type="text"
-            {...registerCategory}
-            innerRef={categoryRef}
-            invalid={!!errors.category}
-          />
-          {errors?.category && (
-            <FormFeedback>{errors.category.message}</FormFeedback>
-          )}
-        </FormGroup>
-        <Button type="submit" color="primary">
-          Save
-        </Button>
-      </Form>
-      <DevTool control={control}></DevTool>
-    </div>
+    <Card
+      style={{
+        width: "18rem",
+      }}
+    >
+      <CardBody>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <CardHeader hidden={isEdit}>{defaultValues.name}</CardHeader>
+          <FormGroup hidden={!isEdit}>
+            <Label for={`name${id}`}>Name</Label>
+            <Input
+              id={`name${id}`}
+              name="name"
+              placeholder="Skill Name"
+              type="text"
+              {...registerName}
+              innerRef={nameRef}
+              invalid={!!errors.name}
+            />
+            {errors?.name && (
+              <FormFeedback> {errors?.name?.message}</FormFeedback>
+            )}
+          </FormGroup>
+
+          <ListGroup flush>
+            <ListGroupItem hidden={isEdit}>
+              {defaultValues.category}
+            </ListGroupItem>
+            <FormGroup hidden={!isEdit}>
+              <Label for={`category${id}`}>Category</Label>
+              <Input
+                id={`category${id}`}
+                name="category"
+                placeholder="Category"
+                type="select"
+                {...registerCategory}
+                innerRef={categoryRef}
+                invalid={!!errors.category}
+              >
+                {categories.map((category) => (
+                  <option>{category.name}</option>
+                ))}
+              </Input>
+              {errors?.category && (
+                <FormFeedback>{errors.category.message}</FormFeedback>
+              )}
+            </FormGroup>
+            <ListGroupItem hidden={isEdit}>
+              {defaultValues.proficiency}
+            </ListGroupItem>
+            <FormGroup hidden={!isEdit}>
+              <Label for={`proficiency${id}`}>Proficiency</Label>
+              <Input
+                id={`proficiency${id}`}
+                name="proficiency"
+                placeholder="proficiency"
+                type="select"
+                {...registerProficiency}
+                innerRef={proficiencyRef}
+                invalid={!!errors.proficiency}
+              >
+                {proficiencies.map((proficiency) => (
+                  <option>{proficiency.name}</option>
+                ))}
+              </Input>
+              {errors?.proficiency && (
+                <FormFeedback>{errors.proficiency.message}</FormFeedback>
+              )}
+            </FormGroup>
+          </ListGroup>
+
+          <Container>
+            <Row xs="4">
+              <Col>
+                <Button type="submit" color="primary" hidden={!isEdit}>
+                  Save
+                </Button>
+              </Col>
+              <Col>
+                <Button color="primary" onClick={handleIsEdit} hidden={isEdit}>
+                  Edit
+                </Button>
+              </Col>
+              <Col>
+                <Button color="danger" hidden={!isEdit}>
+                  Delete
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </Form>
+        <DevTool control={control}></DevTool>
+      </CardBody>
+    </Card>
   );
 }
 
--- Project1/ClientApp/src/components/Skills/Skills.js
+++ Project1/ClientApp/src/components/Skills/Skills.js
@@ -8,7 +8,18 @@ function Skills() {
   return (
     <div>
       <h1>Skills</h1>
-      <SkillForm></SkillForm>
+      {skillsData.skills.map((skill) => (
+        <SkillForm
+          skill={skill}
+          id={3}
+          categories={skillsData.Category}
+          proficiencies={skillsData.Proficiency}
+        />
+      ))}
+      <SkillForm
+        categories={skillsData.Category}
+        proficiencies={skillsData.Proficiency}
+      ></SkillForm>
     </div>
   );
 }
